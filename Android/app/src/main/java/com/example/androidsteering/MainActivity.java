5fd677e25efebbb2494cf6e21b560c2d1062f88b
teamclouday <teamclouday@gmail.com>
2019-12-04 00:15:04 -0500

    update functions

--- Android/app/src/main/java/com/example/androidsteering/MainActivity.java
+++ Android/app/src/main/java/com/example/androidsteering/MainActivity.java
@@ -21,13 +21,30 @@ import org.w3c.dom.Text;
 
 import java.util.Locale;
 
+enum MotionSteering
+{
+    LEFT,
+    NONE,
+    RIGHT
+}
+
+enum MotionAcceleration
+{
+    FORWARD,
+    NONE,
+    BACKWARD
+}
+
 public class MainActivity extends AppCompatActivity
 {
-
-    private SensorManager sensorManager;
     private TextView txtViewDebug;
     private TextView txtViewBTH;
-    private MyService service;
+
+    private SensorManager sensorManager;
+    private MySensorService serviceSensor;
+    private Handler motionHandler;
+    private Runnable motionRunner;
+
     private BluetoothAdapter bthAdapter;
     private BluetoothSocket bthSocket;
 
@@ -44,7 +61,9 @@ public class MainActivity extends AppCompatActivity
         // create sensor manager and sensor
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         // create service object
-        service = new MyService();
+        serviceSensor = new MySensorService();
+        // create motion handler
+        motionHandler = new Handler();
 
         // set main view
         setContentView(R.layout.main);
@@ -54,24 +73,60 @@ public class MainActivity extends AppCompatActivity
         // get bluetooth text view
         txtViewBTH = findViewById(R.id.textViewBTH);
 
-        service.start();
+        serviceSensor.start();
+
+        motionRunner = new Runnable() {
+            @Override
+            public void run() {
+                String motionSteer;
+                switch(serviceSensor.motionSteering)
+                {
+                    case LEFT:
+                        motionSteer = "Left";
+                        break;
+                    case RIGHT:
+                        motionSteer = "Right";
+                        break;
+                    default:
+                        motionSteer = "None";
+                        break;
+                }
+                String motionAcc;
+                switch(serviceSensor.motionAcceleration)
+                {
+                    case FORWARD:
+                        motionAcc = "Forward";
+                        break;
+                    case BACKWARD:
+                        motionAcc = "Backward";
+                        break;
+                    default:
+                        motionAcc = "None";
+                        break;
+                }
+                txtViewDebug.setText("Steering: "+motionSteer+"\n"+"Acceleration: "+motionAcc);
+                motionHandler.postDelayed(this, 20);
+            }
+        };
     }
 
     @Override
     public void onResume()
     {
         super.onResume();
-        service.start();
+        serviceSensor.start();
+        motionHandler.postDelayed(motionRunner, 0);
     }
 
     @Override
     public void onPause()
     {
         super.onPause();
-        service.stop();
+        serviceSensor.stop();
+        motionHandler.removeCallbacksAndMessages(motionRunner);
     }
 
-    class MyService implements SensorEventListener
+    class MySensorService implements SensorEventListener
     {
         private Sensor accSensor;
         private Sensor magSensor;
@@ -79,10 +134,15 @@ public class MainActivity extends AppCompatActivity
         private final float[] accReading = new float[3];
         private final float[] magReading = new float[3];
         private final float[] rotationMatrix = new float[9];
-        private final float[] remapMatrix = new float[9];
         private final float[] orientationMatrix = new float[3];
 
-        public MyService()
+        public MotionAcceleration motionAcceleration = MotionAcceleration.NONE;
+        public MotionSteering motionSteering = MotionSteering.NONE;
+
+        public int motionPitch = 0;
+        public int motionRoll = 0;
+
+        public MySensorService()
         {
             accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -91,8 +151,8 @@ public class MainActivity extends AppCompatActivity
         public void start()
         {
             // sample period is set to 10ms
-            sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-            sensorManager.registerListener(this, magSensor, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+            sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(this, magSensor, SensorManager.SENSOR_DELAY_FASTEST);
         }
 
         public void stop()
@@ -122,8 +182,35 @@ public class MainActivity extends AppCompatActivity
 
             int pitch = (int)Math.toDegrees(orientationMatrix[1]);
             int roll = (int)Math.abs(Math.toDegrees(orientationMatrix[2]))-90;
-            String message = String.format(Locale.ENGLISH, "Pitch:\t%d\nRoll:\t%d", pitch, roll);
-            txtViewDebug.setText(message);
+
+            // update motion steering
+            if(pitch > 15 && pitch < 80)
+            {
+                motionSteering = MotionSteering.LEFT;
+            }
+            else if(pitch < -15 && pitch > -80)
+            {
+                motionSteering = MotionSteering.RIGHT;
+            }
+            else
+            {
+                motionSteering = MotionSteering.NONE;
+            }
+            motionPitch = pitch;
+
+            if(roll < -20 && roll > -80)
+            {
+                motionAcceleration = MotionAcceleration.FORWARD;
+            }
+            else if(roll > 10 && roll < 40)
+            {
+                motionAcceleration = MotionAcceleration.BACKWARD;
+            }
+            else
+            {
+                motionAcceleration = MotionAcceleration.NONE;
+            }
+            motionRoll = roll;
         }
 
         @Override
